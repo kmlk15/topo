b16b03c9f6fd799f9570f10ab18235edc6a9ae2e
terencelui <terence@tmdstudio.com>
2011-11-23 19:40:15 -0800

    added itinerary and profile pages, work in progress

---
+++ app/controllers/app/Itinerary.java
@@ -0,0 +1,11 @@
+package controllers.app;
+
+import play.mvc.Controller;
+
+public class Itinerary extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
---
+++ app/controllers/app/Profile.java
@@ -0,0 +1,11 @@
+package controllers.app;
+
+import play.mvc.Controller;
+
+public class Profile extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
