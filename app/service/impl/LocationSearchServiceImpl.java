e06f68d66b693f4a7c628cc044f1904ea3fa6270
Keith Lui <kmlk15@gmail.com>
2011-11-24 18:27:48 -0800

    Auto Complete only has level one Location look up is by level one

--- app/service/impl/LocationSearchServiceImpl.java
+++ app/service/impl/LocationSearchServiceImpl.java
@@ -16,18 +16,21 @@ import service.api.LocationSearchService;
 
 public class LocationSearchServiceImpl implements LocationSearchService {
 
+	private static int PAGE_SIZE = 15;
+	
 	public List<Location> getRecommendedLocations(User u, SearchQuery q) {
 		List<Score> scores = null;
 		if (q.hasCategory()) {
-			scores = Score.find("select s from Score s join s.category c where s.user = :uid and c.id in :cids order by score desc").bind("uid", u).bind("cids", q.getCategories()).fetch(9);			
+			List<Long> levelTwo = getLevelTwoCategories(q.getCategories());
+			scores = Score.find("select s from Score s join s.category c where s.user = :uid and c.id in :cids order by score desc").bind("uid", u).bind("cids", levelTwo).fetch(PAGE_SIZE);			
 		} else {
-			scores = Score.find("select s from Score s join s.category c where s.user = :uid and c.level = 2 order by score desc").bind("uid", u).fetch(9);
+			scores = Score.find("select s from Score s join s.category c where s.user = :uid and c.level = 2 order by score desc").bind("uid", u).fetch(PAGE_SIZE);
 		}
 		if (CollectionUtils.isNotEmpty(scores)) {
 			List<Location> locations = new ArrayList<Location>();
 			Iterator<Score> iter = scores.iterator();
-			int count = 4;
-			while (iter.hasNext() && locations.size() < 9) {
+			int count = 5;
+			while (iter.hasNext() && locations.size() < PAGE_SIZE) {
 				Score s = iter.next();
 				List<Location> l = null;
 				if (q.hasCity()) {
@@ -44,9 +47,9 @@ public class LocationSearchServiceImpl implements LocationSearchService {
 		} else if (q.hasCategory()) {
 			List<Location> locations = new ArrayList<Location>();
 			if (q.hasCity()) {
-				locations = Location.find("select distinct l from Location l join l.category c join l.address a where c.id = :cid and a.city.id in :aids").bind("cid", q.getCategories()).bind("aids", q.getCities()).fetch(9);	
+				locations = Location.find("select distinct l from Location l join l.category c join l.address a where c.id = :cid and a.city.id in :aids").bind("cid", q.getCategories()).bind("aids", q.getCities()).fetch(PAGE_SIZE);	
 			} else {
-				locations = Location.find("select distinct l from Location l join l.category c where c.id = :cid").bind("cid", q.getCategories()).fetch(9);
+				locations = Location.find("select distinct l from Location l join l.category c where c.id = :cid").bind("cid", q.getCategories()).fetch(PAGE_SIZE);
 			}
 			return locations;
 		}
@@ -61,25 +64,25 @@ public class LocationSearchServiceImpl implements LocationSearchService {
 		}
 		List<Location> locations = null;
 		if (q.hasCity() && q.hasCategory()) {
-			locations = Location.find("select distinct l from Location l join l.category c join l.address a where c.id in :cids and a.city.id in :aids order by popularity desc").bind("cids", cids).bind("aids", q.getCities()).fetch(9);
+			locations = Location.find("select distinct l from Location l join l.category c join l.address a where c.id in :cids and a.city.id in :aids order by popularity desc").bind("cids", cids).bind("aids", q.getCities()).fetch(PAGE_SIZE);
 		} else if (q.hasCity()) {
-			locations = Location.find("select distinct l from Location l join l.address a where a.city.id in :aids order by popularity desc").bind("aids", q.getCities()).fetch(9);
+			locations = Location.find("select distinct l from Location l join l.address a where a.city.id in :aids order by popularity desc").bind("aids", q.getCities()).fetch(PAGE_SIZE);
 		} else if (q.hasCategory()) {
-			locations = Location.find("select distinct l from Location l join l.category c where c.id in :cids order by popularity desc").bind("cids", cids).fetch(9);
+			locations = Location.find("select distinct l from Location l join l.category c where c.id in :cids order by popularity desc").bind("cids", cids).fetch(PAGE_SIZE);
 		} else {
-			locations = Location.find("order by popularity desc").fetch(9);
+			locations = Location.find("order by popularity desc").fetch(PAGE_SIZE);
 		}
 		return locations;
 	}
 
 	public List<Location> getObscureLocations(User u, SearchQuery q) {
-		List<Long> levelOne = new ArrayList<Long>();
+		List<Long> levelTwo = new ArrayList<Long>();
 		List<Score> scores = null;
 		if (q.hasCategory()) {
-			levelOne = getLevelOneCategories(q.getCategories());
-			scores = Score.find("select s from Score s join s.category c where s.user = :uid and s.score > 0 and c.id in :cids order by s.score desc ").bind("uid", u).bind("cids", q.getCategories()).fetch(9);
+			levelTwo = getLevelTwoCategories(q.getCategories());
+			scores = Score.find("select s from Score s join s.category c where s.user = :uid and s.score > 0 and c.id in :cids order by s.score desc ").bind("uid", u).bind("cids", levelTwo).fetch(PAGE_SIZE);
 		} else {
-			scores = Score.find("select s from Score s where s.user = :uid and s.score > 0 order by s.score desc ").bind("uid", u).fetch(9);
+			scores = Score.find("select s from Score s where s.user = :uid and s.score > 0 order by s.score desc ").bind("uid", u).fetch(PAGE_SIZE);
 		}
 		if (CollectionUtils.isNotEmpty(scores) || q.hasCategory()) {
 			List<Long> cids = new ArrayList<Long>();
@@ -87,19 +90,19 @@ public class LocationSearchServiceImpl implements LocationSearchService {
 				cids.add(s.category.id);
 			}
 			if (CollectionUtils.isEmpty(cids)) {
-				cids.addAll(q.getCategories());
+				cids.addAll(levelTwo);
 			}
 			List<Location> locations = null;
 			if (q.hasCity() && q.hasCategory()) {
-				List<Location> levelOneLocations = Location.find("select distinct l from Location l join l.category c join l.address a where c.id in :levelOnes and a.city.id in :aids order by l.popularity asc").bind("levelOnes", levelOne).bind("aids", q.getCities()).fetch(9);
+				List<Location> levelOneLocations = Location.find("select distinct l from Location l join l.category c join l.address a where c.id in :levelOnes and a.city.id in :aids order by l.popularity asc").bind("levelOnes", q.getCategories()).bind("aids", q.getCities()).fetch(PAGE_SIZE);
 				locations = handleObscureWithCategories(levelOneLocations, cids);
 			} else if (q.hasCity()) {
-				locations = Location.find("select distinct l from Location l join l.category c join l.address a where c.id not in :cids and a.city.id in :aids order by l.popularity asc").bind("cids", cids).bind("aids", q.getCities()).fetch(9);
+				locations = Location.find("select distinct l from Location l join l.category c join l.address a where c.id not in :cids and a.city.id in :aids order by l.popularity asc").bind("cids", cids).bind("aids", q.getCities()).fetch(PAGE_SIZE);
 			} else if (q.hasCategory()) {
-				List<Location> levelOneLocations = Location.find("select l from Location l join l.category c where c.id in :levelOnes order by l.popularity asc").bind("levelOnes", levelOne).fetch();
+				List<Location> levelOneLocations = Location.find("select l from Location l join l.category c where c.id in :levelOnes order by l.popularity asc").bind("levelOnes", q.getCategories()).fetch();
 				locations = handleObscureWithCategories(levelOneLocations, cids);
 			} else {
-				locations = Location.find("select distinct l from Location l join l.category c where c.id not in :cids order by l.popularity asc").bind("cids", cids).fetch(9);
+				locations = Location.find("select distinct l from Location l join l.category c where c.id not in :cids order by l.popularity asc").bind("cids", cids).fetch(PAGE_SIZE);
 			}
 			return locations;
 		}
@@ -115,10 +118,20 @@ public class LocationSearchServiceImpl implements LocationSearchService {
 		}
 		return levelOne;
 	}
+
+	private List<Long> getLevelTwoCategories(List<Long> categories) {
+		List<Long> levelTwo = new ArrayList<Long>();
+		for (Long i : categories) {
+			List<Long> lids = Location.find("select l.id from Location l join l.category c where c.id = :cid").bind("cid", i).fetch();				
+			List<Long> ids = Location.find("select c.id from Location l join l.category c where l.id in :lids and c.level = 2").bind("lids", lids).fetch();
+			levelTwo.addAll(ids);
+		}
+		return levelTwo;
+	}
 	
 	private List<Location> handleObscureWithCategories(List<Location> levelOneLocations, List<Long> cids) {
 		List<Location> locations = new ArrayList<Location>();
-		for (int i = 0; i < levelOneLocations.size() && locations.size() < 9; i++) {
+		for (int i = 0; i < levelOneLocations.size() && locations.size() < PAGE_SIZE; i++) {
 			Location l = levelOneLocations.get(i);
 			boolean include = true;
 			for (Category c : l.category) {
